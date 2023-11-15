01f349c794613ba8df18f2b014db7cda79749977
ana <ana@anniecannons.com>
2023-11-15 12:01:03 -0800

    final test

--- src/components/WeatherCards.js
+++ src/components/WeatherCards.js
@@ -8,8 +8,14 @@ function WeatherCard(props) {
         <div className="imageContainer">
         <img src={props.image} alt="Weather" />
         <p>{props.name}</p>
+        <div className="spanDiv">
         <span className="headingP">{props.heading}</span>
         </div>
+
+        </div>
+        
+        
+       
       
       </div>
     );
